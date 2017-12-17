5e910dc33047cea47ced06d9a0fb30f336f8c4fc
lbors <lucas_borges95@yahoo.com.br>
2017-12-17 08:39:10 -0300

    Update ns3-script.sh

--- scripts/ns3-script.sh
+++ scripts/ns3-script.sh
@@ -15,5 +15,24 @@ do
         mv /home/ns3/Documents/ns-allinone-3.26/ns-3.26/csma-dash* /home/ns3/Documents/$nome/	
         mv /home/ns3/Documents/ns-allinone-3.26/ns-3.26/wifi-dash* /home/ns3/Documents/$nome/
         sleep 1
+	
+	echo "Separando arquivos WiFi"
+	cd /home/ns3/Documents/$nome/
+	mkdir wifi
+	mkdir wifi/Trace
+	mkdir wifi/Wire
+	mv /home/ns3/Documents/$nome/wifi-dash* /home/ns3/Documents/$nome/wifi/
+	mv /home/ns3/Documents/$nome/wifi/*.pcap /home/ns3/Documents/$nome/wifi/Wire/
+	mv /home/ns3/Documents/$nome/wifi/*.tr /home/ns3/Documents/$nome/wifi/Trace/
+	sleep 1
+
+	echo "Separando arquivos CSMA"
+	mkdir csma
+	mkdir csma/Trace
+	mkdir csma/Wire
+	mv /home/ns3/Documents/$nome/csma-dash* /home/ns3/Documents/$nome/csma/
+	mv /home/ns3/Documents/$nome/csma/*.pcap /home/ns3/Documents/$nome/csma/Wire/
+	mv /home/ns3/Documents/$nome/csma/*.tr /home/ns3/Documents/$nome/csma/Trace/
+	sleep 1
 done
 
